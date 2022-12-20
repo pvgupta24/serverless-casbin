89e21ec2d817b6f3c1df98d0e1d7894e14f5a535
Praveen Gupta <pvgupta24@gmail.com>
2022-12-20 15:26:21 -0800

    Added utils for workloads

---
+++ ecommerce/src/utils.js
@@ -0,0 +1,62 @@
+const aws = require('aws-sdk')
+
+// ================================================================== //
+const _getSNS = () => {
+  return new aws.SNS();
+
+  // FIXME: Add Offline support
+  const snsOffline = {
+    endpoint: "http://127.0.0.1:4002",
+  }
+  return process.env.IS_OFFLINE ? new aws.SNS(snsOffline) : new aws.SNS();
+};
+
+const _getTopicArn = (functionName) => {
+  return process.env[functionName + 'SnsTopicArn'];
+
+  // FIXME: Add Offline support
+  return process.env.IS_OFFLINE
+    ? "arn:aws:sns:us-east-1:123456789012:" + functionName + "Topic"
+    : process.env[functionName + 'SnsTopicArn'];
+}
+
+// ================================================================== //
+// Helper Functions
+module.exports._getConfig = fileName => __dirname + '/config/' + fileName;
+module.exports._printDebug = variable => console.log('DEBUG: Variable', variable, 'of type', typeof (variable));
+module.exports._printFunctionStage =
+  (functionName, event, stage) => console.log(stage, 'function:', functionName, 'with params', event);
+module.exports.sleep = ms => new Promise(r => setTimeout(r, ms));
+
+// ======================================================================================= //
+// Helper function to trigger the nextfunction in message from the currfunction
+module.exports.triggerNextFunction = (message, callback, currFunction, response = null) => {
+  _printDebug(message);
+  _printDebug(message.useMonitor);
+  _printDebug(message.nextFunction);
+  const sns = _getSNS();
+  // Whether to use intermediate monitor function or not
+  const nextFunction = (currFunction !== 'monitor' && message.useMonitor)
+    ? "monitor" : message.nextFunction;
+  _printDebug(nextFunction);
+  console.log(`Triggering ${nextFunction} from ${currFunction}\ with topic
+    ${_getTopicArn(nextFunction)} & useMonitor: - ${message.useMonitor}`);
+
+  const opts = {
+    Message: JSON.stringify(message),
+    TopicArn: _getTopicArn(nextFunction),
+  };
+
+  sns.publish(opts, (err, data) => {
+    if (err) {
+      console.log('ERR: error while sending message over sns:', err)
+      callback(err, null)
+    } else {
+      const res = response ?? {
+        statusCode: 200,
+        body: 'Message sent to ' + nextFunction
+      };
+      callback(null, res);
+    }
+  })
+}
